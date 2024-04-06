a8c85f2a8ad38291b596fe1968a3a7ea21298600
Matt Fortuna <65244897+mattfortuna@users.noreply.github.com>
2024-04-05 17:35:00 -0700

    Update creativeScoring.js

--- creativeScoring.js
+++ creativeScoring.js
@@ -7,16 +7,20 @@ const audience = 'tech'; // this can be made to be variable as well
 
 // Calculate the score of each creative
 export const calculateCreativeScores = (creatives, audience) => {
+  // return array of creatives with updated scores
     return creatives
       .filter(creative => creative.segmentAudience === audience)
       .map(creative => {
-        const clickWeight = calculateClickWeight(creative.clicks, creative.impressions); // calculate percentage of clicks per impression
-        const engagementWeight = calculateEngagementWeight(creative.engagements, creative.impressions); // calculate percentage of engangements per impression
-        const timeWeight = calculateTimeWeight(creative.timePeriod); // calculate influence of time of day
+        // calculate percentage of clicks per impression
+        const clickWeight = calculateClickWeight(creative.clicks, creative.impressions);
+        // calculate percentage of engangements per impression
+        const engagementWeight = calculateEngagementWeight(creative.engagements, creative.impressions);
+        // calculate influence of time of day
+        const timeWeight = calculateTimeWeight(creative.timePeriod);
   
         const score = (clickWeight + engagementWeight) * timeWeight; // calculate score
         return { ...creative, score }; // return creative
-      }); // return array of creatives with updated scores
+      });
   };
 
 // Select best of scored creatives
@@ -26,12 +30,3 @@ export const highestPerformingCreative = (scoredCreatives) => {
   }
   return scoredCreatives.reduce((prev, current) => (prev.score > current.score) ? prev : current);
 };
-
-// Example case
-const scoredCreatives = calculateCreativeScores(creatives, audience);
-const bestCreative = highestPerformingCreative(scoredCreatives);
-if (bestCreative) {
-  console.log(`The best creative for '${audience}' segment is:`, bestCreative);
-} else {
-  console.log(`No creatives available for '${audience}' segment.`);
-}
