013a404b918242037c774e8066c05fe526793d52
Matt Fortuna <65244897+mattfortuna@users.noreply.github.com>
2024-04-05 17:38:13 -0700

    Update exampleCreative.js

--- exampleCreative.js
+++ exampleCreative.js
@@ -1,8 +1,9 @@
 import { calculateCreativeScores, highestPerformingCreative } from './creativeScoring.js';
+import { creatives } from './creatives.js';
 
 // test out an example creative
 
-const scoredCreatives = calculateCreativeScores(creatives, audience);
+const scoredCreatives = calculateCreativeScores(creatives, 'tech');
 const bestCreative = highestPerformingCreative(scoredCreatives);
 if (bestCreative) {
   console.log(`The best creative for '${audience}' segment is:`, bestCreative);
